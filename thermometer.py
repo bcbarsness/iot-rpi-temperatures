d03a3c3fd3b4042adfe5a0b00a014bc3f71d856f
Brendan Barsness <bcbarsness@gmail.com>
2015-07-27 10:12:56 -0400

    cleaned up code

--- thermometer.py
+++ thermometer.py
@@ -8,7 +8,8 @@ import json
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
+update_rate_secs = 60 
 base_dir = '/sys/bus/w1/devices/'
 #device_folder = glob.glob(base_dir + '28*')[0]
 #device_file = device_folder + '/w1_slave'
@@ -16,34 +17,25 @@ device_temp1 = glob.glob(base_dir + '28-000006dd6544/w1_slave') [0]
 device_temp2 = glob.glob(base_dir + '28-000006dc7b83/w1_slave') [0]
 url="http://api.openweathermap.org/data/2.5/weather?zip=23507,us"
  
-def read_temp_raw():
-    f1 = open(device_temp1, 'r')
-    lines1 = f1.readlines()
-    f1.close()
+def read_temp_raw(device_id):
+    temp_file = open(device_id, 'r')
+    lines = temp_file.readlines()
+    temp_file.close()
     
-    f2 = open(device_temp2, 'r')
-    lines2 = f2.readlines()
-    f2.close()
-
-    return (lines1, lines2)
+    return (lines)
  
-def read_temp():
-    lines1, lines2 = read_temp_raw()
-    while (lines1[0].strip()[-3:] != 'YES') and (lines2[0].strip()[-3:] != 'YES') :
+def read_temp(device_id):
+    lines = read_temp_raw(device_id)
+    while (lines[0].strip()[-3:] != 'YES') :
         time.sleep(0.2)
-        lines1, lines2 = read_temp_raw()
-    equals_pos1 = lines1[1].find('t=')
-    equals_pos2 = lines2[1].find('t=')
-    if (equals_pos1 != -1) and (equals_pos2 != -1) :
-        temp_string = lines1[1][equals_pos1+2:]
+        lines = read_temp_raw(device_id)
+    equals_pos = lines[1].find('t=')
+    if (equals_pos != -1) :
+        temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f1 = temp_c * 9.0 / 5.0 + 32.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
 
-        temp_string = lines2[1][equals_pos2+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f2 = temp_c * 9.0 / 5.0 + 32.0
-
-        return (temp_f1, temp_f2)
+        return (temp_f)
 
 def getOutsideTemp():
 
@@ -65,13 +57,14 @@ def main():
 
     while True:
         try:
-            myTemp1, myTemp2 = read_temp()
+            temp_sensor1 = read_temp(device_temp1)
+            temp_sensor2 = read_temp(device_temp2)            
             outsideTemp = getOutsideTemp()
-            print 'Temp1: %s Temp2: %s Outside: %s' % (myTemp1, myTemp2, outsideTemp)
-            f = urllib2.urlopen(baseURL + "&field1=%s&field2=%s&field3=%s" % (myTemp1, myTemp2, outsideTemp))
+            print 'Temp1: %s Temp2: %s Outside: %s' % (temp_sensor1, temp_sensor2, outsideTemp)
+            f = urllib2.urlopen(baseURL + "&field1=%s&field2=%s&field3=%s" % (temp_sensor1, temp_sensor2, outsideTemp))
             print f.read()
             f.close()	
-	    time.sleep(60)
+	        time.sleep(update_rate_secs)
         except:
             print 'exiting.'
             break
